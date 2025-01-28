343eebd564fb8b967292bbf6cfe84849b0c56ea2
Marija Taseva <99970406+mtaseva@users.noreply.github.com>
2025-01-28 23:17:09 +0100

    Final touches

--- Gravitacija.java
+++ Gravitacija.java
@@ -4,21 +4,21 @@ public class Gravitacija {
 
 public static double pospesek (double v){
 
-
-double M = 5.972 * Math.pow(10,24);
-double C = 6.674 * Math.pow(10,-11);
-double r = 6.371 * Math.pow(10, 6);
-
-double a = (C * M )/((r+v)*(r+v));
-return a ;
+  double M = 5.972 * Math.pow(10,24);
+  double C = 6.674 * Math.pow(10,-11);
+  double r = 6.371 * Math.pow(10, 6);
+  
+  double a = (C * M )/((r+v)*(r+v));
+  return a ;
 }
 public static void main(String [] args){
+  
+  Scanner sc = new Scanner(System.in);
+  System.out.println("Vnesete visina (v metrih) ");
+  int visina = sc.nextInt();
 
-Scanner sc = new Scanner(System.in);
-System.out.println("Vnesete visina ");
-int visina = sc.nextInt();
-System.out.printf("Pospesek je enak %f", Gravitacija.pospesek(visina));
-System.out.printf("Visina je enaka %f", visina);
-System.out.printf("Gravitacijski pospesek je enak%f", M);
-}
+  double pospesek = Gravitacija.pospesek(visina);
+  System.out.printf("Pospesek je enak: %.6f m/s^2%n", pospesek);
+  System.out.printf("Visina je enaka: %d m%n", visina);
+  }
 }
